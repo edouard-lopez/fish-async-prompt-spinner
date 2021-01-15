b2ad3aa966be3f6b5c153d7dbfe2bb06b7770c41
Edouard Lopez <contact@edouard-lopez.com>
2021-01-15 16:14:19 +0100

    merge spinner in plugin file, add uninstall hook

--- conf.d/fish-async-prompt-spinner.fish
+++
@@ -1,5 +0,0 @@
-function _fish-async-prompt-spinner --on-event fish-async-prompt-spinner
-    set --names \
-        | string replace --filter --regex '(^_spinner_)' 'set --erase $1' \
-        | source
-end
